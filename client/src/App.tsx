1b1ce251a3d5b18210b85f9bfa4bac9291719ea4
Eray Alp <erayalp.e@gmail.com>
2024-05-29 14:34:14 +0300

    add react-router and navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,29 @@
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { themeSettings } from "./theme";
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "@/scenes/navbar/index";
+import Dashboard from "@/scenes/dashboard/index";
+import Predictions from "@/scenes/predictions/index";
 
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
 
   return <div className="app">
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/predictions" element={<Predictions />} />
+          </Routes>
+        </Box>
+      </ThemeProvider>
+    </BrowserRouter>
   </div>
 }
 
