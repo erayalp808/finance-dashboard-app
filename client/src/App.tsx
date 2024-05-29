bb9d5b8649f4b469cad8175ed9586102e0673644
Eray Alp <erayalp.e@gmail.com>
2024-05-29 15:06:12 +0300

    set dashboard layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,9 @@ import { useMemo } from "react";
 import { themeSettings } from "./theme";
 import { Box, CssBaseline, ThemeProvider } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import NavBar from "@/scenes/navbar/index";
-import Dashboard from "@/scenes/dashboard/index";
-import Predictions from "@/scenes/predictions/index";
-
+import NavBar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard"; 
+//import Predictions from "@/scenes/predictions/index";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
@@ -19,7 +18,7 @@ function App() {
           <NavBar />
           <Routes>
             <Route path="/" element={<Dashboard />} />
-            <Route path="/predictions" element={<Predictions />} />
+            {/*<Route path="/predictions" element={<Predictions />} />*/}
           </Routes>
         </Box>
       </ThemeProvider>
--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import { Box, useTheme } from '@mui/material';
 
-type Props = {}
+type Props = {};
+
+const gridTemplate = `
+    "a d g"
+    "a d g"
+    "a d g"
+    "a d h"
+    "b e h"
+    "b e h"
+    "b f i"
+    "c f i"
+    "c f j"
+    "c f j"
+`;
 
 const Dashboard = (props: Props) => {
-  return (
-    <div>Dashboard</div>
+    const { palette } = useTheme();
+    return (
+        <Box width="100%" height="100%" display="grid" gap="1.5rem"
+            sx={{
+                gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+                gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+                gridTemplateAreas: gridTemplate,
+            }}
+        >
+            <Box bgcolor="#fff" gridArea="a"></Box>
+            <Box bgcolor="#fff" gridArea="b"></Box>
+            <Box bgcolor="#fff" gridArea="c"></Box>
+            <Box bgcolor="#fff" gridArea="d"></Box>
+            <Box bgcolor="#fff" gridArea="e"></Box>
+            <Box bgcolor="#fff" gridArea="f"></Box>
+            <Box bgcolor="#fff" gridArea="g"></Box>
+            <Box bgcolor="#fff" gridArea="h"></Box>
+            <Box bgcolor="#fff" gridArea="i"></Box>
+            <Box bgcolor="#fff" gridArea="j"></Box>
+        </Box>
   )
 }
 
